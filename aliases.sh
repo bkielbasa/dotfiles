dae5b8040177a81871fea0a30f3d086c40d87cac
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2019-01-29 12:51:08 +0100

    add aliases

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+# git
+alias push='git push'
+alias pull='git pull'
+alias fpush='git push -f'
+alias fpull='git pull -f'
+alias gc='git commit -S'
+alias reb='g rebase -i master'
+
+# brainly
+alias tests='./script/test'
+alias stop='./script/stop'
+
+# vim aliases
+bindkey "[D" backward-word
+bindkey "[C" forward-word
+bindkey "^[a" beginning-of-line
+bindkey "^[e" end-of-line
+
